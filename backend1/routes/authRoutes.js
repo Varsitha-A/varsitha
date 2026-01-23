f24b1e9b50360ec92b0eef9d14160aacc3b4af86
Varsitha-A <varsithaarun2409@gmail.com>
2026-01-23 09:05:00 +0530

    ADDED

--- backend1/routes/authRoutes.js
+++ backend1/routes/authRoutes.js
@@ -1,7 +1,6 @@
-const express=require('express')
-const router=express.Router()
-
-const{register, login}=require('../controllers/authControllers')
-router.post('/register',register)
-router.post('/login',login)
-module.exports=router
+const express = require('express');//importing express module
+const router = express.Router(); //creating a router object using express
+const {register,login} = require('../controllers/authControllers');//importing register and login functions from authControllers
+router.post('/register', register);//only register route is enabled
+router.post('/login', login);//only login route is enabled
+module.exports = router;
